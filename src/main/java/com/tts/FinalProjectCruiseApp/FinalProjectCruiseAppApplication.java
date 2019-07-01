c3aa03b5e8053f04ac5d0b94a6d0f099ae8b05f5
Grady Hodge <codebossmonkey@gmail.com>
2019-07-01 14:39:21 -0500

    initial commit

---
+++ src/main/java/com/tts/FinalProjectCruiseApp/FinalProjectCruiseAppApplication.java
@@ -0,0 +1,13 @@
+package com.tts.FinalProjectCruiseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCruiseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCruiseAppApplication.class, args);
+	}
+
+}
